d2a69a33c3acce9b873c40db4a41e115de7552cc
drapkinamaria <drapkinamaria02@gmail.com>
2024-08-06 13:15:13 +0500

    download resume

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
 import { setupEditable } from './editable.js';
 import { setupMaterialWave } from './materialWave.js';
 import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 function downloadPDF() {
-    const { jsPDF } = window.jspdf;
-
-    const doc = new jsPDF();
-
     const content = document.getElementById('app');
 
-    doc.html(content, {
-        callback: function (doc) {
-            doc.save('resume.pdf');
-        },
-        x: 10,
-        y: 10
+    html2canvas(content, { scale: 2 }).then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF('p', 'mm', 'a4');
+
+        const imgWidth = 210;
+        const pageHeight = 297;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        let heightLeft = imgHeight;
+        let position = 0;
+
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+
+        while (heightLeft >= 0) {
+            position = heightLeft - imgHeight;
+            pdf.addPage();
+            pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+            heightLeft -= pageHeight;
+        }
+
+        pdf.save('resume.pdf');
     });
 }
 
